8334be693a349bb711431abd65e4bafd65387101
joshuamgoodwin <joshua.m.goodwin@gmail.com>
2017-04-06 08:04:37 -0400

    update forms

--- Ohio Legal Services Assistant/Constants.swift
+++ Ohio Legal Services Assistant/Constants.swift
@@ -20,6 +20,6 @@ struct Arrays {
     
     static let foodStampVersions : [String] = ["10-01-2016", "10-01-2015 to 10-01-2016", "10-01-2014 to 10-01-2015"]
     
-    static let OWFVersion : [String] = ["07-16 to present", "01-15 to 07-16", "07-14 to 01-15"]
+    static let OWFVersion : [String] = ["01-17 to present", "07-16 to 01-17", "01-15 to 07-16", "07-14 to 01-15"]
     
 }
